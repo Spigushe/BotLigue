2357ad0271af8b5fcac089979489d9f6a96d9e8b
Spigushe <martin.cuchet@gmail.com>
2020-11-05 22:34:14 +0100

    BOT: First admin command

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # BotLigue
 Python Discord Bot using Challonge API to manage tournaments
+
+## Requirements
+The bot is based on the following packages and their dependancies
+* `Discord.py`
+* `pychal`
+
+## Installation
+*to be detailed*
+
+## Usage
+### Setting Tokens
+Create a `.env` file at root level. Serve the file with tokens from
+[Discord](https://discord.com/developers/) and from
+[Challonge](https://challonge.com/fr/settings/developer)
+```python
+[discord]
+DISCORD_TOKEN={your-discord-token-here}
+
+[challonge]
+CHALLONGE_NICKNAME={your-challonge-nickname}
+CHALLONGE_TOKEN={your-challonge-token}
+```
+
+## List of commands
+Commands start with `bot ` at the start of the message
+
+### Admin commands
+Admin commands are meant to be executed by `Guild.Member` with `Administrator`
+role. *This has to be implemented*
+
+Hereunder is a list of bot commands. The brackets are not meant to be typed and
+might induce bugs. *This needs to be resolved*
+
+```bash
+bot admin create {Tournament Name} {Tournament Type}
+```
--- bot.py
+++ bot.py
@@ -35,6 +35,40 @@ async def on_message(message):
 	if message.content.lower().startswith("bot "):
 		content = message.content[4:]
 		print(f"Received: {content}")
+		if content.lower().startswith("admin "):
+			"""
+			Administrator commands
+			- CREATE 	Create a new tournament
+			- CHECKIN 	Proceed to check-in
+			- START 	Start tournament
+			"""
+		#	if not message.author.has_role("admin"):
+		#		return
+
+
+			content = message.content[10:] #bot admin /
+			# Administrator Commands
+			if content.lower().startswith("create "):
+				"""
+				Create a new Challonge tournament
+
+				Args:
+					"Tournament Name"
+					"Tournament Type"
+				"""
+				# Dealing with context
+				content = message.content[17:] #bot admin create /
+				args = content.split(" ") #tournament-name tournament-type
+				# Processing args
+				t_name = " ".join(args[0].split("-"))
+				t_url  = "CL_" + "".join(args[0].split("-"))
+				t_type = " ".join(args[1].split("-")).lower()
+				# Create tournament
+				t_json = challonge.tournaments.create(t_name, t_url, t_type)
+				# Saving tournament json file
+				t_file = "tournaments/"+t_url+".json" # tournaments/ is in .gitignore
+				with open(t_file, "w") as write_file:
+					json.dump(t_json, write_file, indent=4, default=str)
 
 			await message.delete()
 
