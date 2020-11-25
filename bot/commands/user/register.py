c18ed977f076d3c9097719db5172fd0e5dd1bbba
Spigushe <martin.cuchet@gmail.com>
2020-11-25 21:30:24 +0100

    Update Helper

--- bot/commands/user/register.py
+++ bot/commands/user/register.py
@@ -13,61 +13,22 @@ async def show_help(message):
 		url="https://github.com/Spigushe/BotLigue/blob/main/Registration.md"
 	)
 	embed.set_thumbnail(url="https://ligue.spigushe.com/Afficher/CSS/Images/Logo.png")
+	embed.set_footer(text=f"List of options for `bot register` command")
 
 	# List of mandatory options
-	embed.add_field(
-		name="Mandatory options",
-		value="Hereunder is the list of mandatory options to register",
-		inline=False
-	)
+	embed.add_field( name="Mandatory options",value="Hereunder is the list of mandatory options to register",inline=False )
+	embed.add_field( name="-decklist",value="Register the deck you are using, please use a link you will not update during the season",inline=True )
+	embed.add_field( name="-hash",value="Announce the hash of your deck, please use a hash you will not update during the season",inline=True )
 
-	#-decklist url_to_decklist
-	embed.add_field(
-		name="-decklist",
-		value="Register the deck you are using, please use a link you will not update during the season",
-		inline=True
-	)
+	# List of facultative options
+	embed.add_field( name="Facultative options",value="Hereunder is the list of facultative options to register",inline=False)
+	embed.add_field( name="-nickname",value="Set your nickname to one different from the one you are using here",inline=True )
+	embed.add_field( name="-cz", value="Help us with naming your command zone here", inline=True )
+	embed.add_field( name="-archetype",value="Help us determining the archetype of your deck",inline=True )
 
-	#-hash generated_hash
-	embed.add_field(
-		name="-hash",
-		value="Announce the hash of your deck, please use a hash you will not update during the season",
-		inline=True
-	)
-
-	# List of optional options
-	embed.add_field(
-		name="Optional options",
-		value="Hereunder is the list of optional options to register",
-		inline=False
-	)
-
-	# -nickname Chosen Nickname
-	embed.add_field(
-		name="-nickname",
-		value="Set your nickname to one different from the one you are using here",
-		inline=True
-	)
-
-	# -cz commander or -cz partner1 / parter2
-	embed.add_field(
-		name="-cz",
-		value="Help us with naming your command zone here (if you are using partners, use a `/` to mark separation)",
-		inline=True
-	)
-
-	# -archetype string
-	embed.add_field(
-		name="-archetype",
-		value="Help us deternmining the archetype",
-		inline=True
-	)
-
-	embed.set_footer(
-        text=f"List of options for `bot register` command",
-		icon_url="https://ligue.spigushe.com/Afficher/CSS/Images/Logo.png"
-    )
 	await message.channel.send(embed=embed)
+
+
 
 	return
 
